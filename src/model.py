637d9dc3cbf744a118f9acae6a075f350dc52814
joemoyes <71090896+moyes-joe@users.noreply.github.com>
2023-07-02 23:22:38 +0100

    Add domain model

---
+++ src/model.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class OutOfStock(Exception):
+    pass
+
+
+def allocate(line: OrderLine, batches: list[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
+class ValueObject(BaseModel):
+    """A value object is any domain object that is uniquely identified by the data it holds.
+
+    We usually make them immutable.
+
+    Value objects do not have an identity, they are defined by the values they hold.
+    """
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.__dict__ == other.__dict__
+
+    class Config:
+        """Freeze the instance.
+
+        For value objects, the hash should be based on all the value attributes,
+        and we should ensure that the objects are immutable.
+        We get this for free by specifying.
+        """
+
+        frozen = True
+
+
+class OrderLine(ValueObject):
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: date | None):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
+
+    def __repr__(self):
+        return f"<Batch {self.reference}>"
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
